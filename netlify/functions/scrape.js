85989f91315964cebf1ee61a2884dbbbb246362f
Filmzone&Flixify <159073116+1056516@users.noreply.github.com>
2024-10-05 09:28:52 -0400

    Update scrape.js

--- netlify/functions/scrape.js
+++ netlify/functions/scrape.js
@@ -2,9 +2,27 @@
 const axios = require('axios');
 
 exports.handler = async (event) => {
-    const { url } = JSON.parse(event.body);
+    // Parsing the request body to get the URL
+    let jsonBody;
+    try {
+        jsonBody = JSON.parse(event.body);
+    } catch (error) {
+        return {
+            statusCode: 400,
+            body: JSON.stringify({ success: false, message: 'Invalid JSON input.' }),
+        };
+    }
+
+    const { url } = jsonBody;
+    if (!url) {
+        return {
+            statusCode: 400,
+            body: JSON.stringify({ success: false, message: 'Missing URL in request.' }),
+        };
+    }
 
     try {
+        // Make a request to the proxy API to scrape the given URL
         const response = await axios.get(`http://149.202.98.190/?url=${encodeURIComponent(url)}`);
         // Assuming the proxy API returns the content directly
         return {
